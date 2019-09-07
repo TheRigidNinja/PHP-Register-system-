c82a439cc4628a27d0342b0e59e1f5f954b85d84
brian shisanya <brian.shisanya2000@gmail.com>
2019-09-07 19:58:52 +1000

    Fixed some of the link in Login and Register Page

---
+++ Templates/SubmitAction.php
@@ -0,0 +1,3 @@
+<input type="submit" class="btn btn-outline-primary form-control text-center">
+
+<a class="btn btn-outline-primary form-control text-center" href="#"><?php echo htmlspecialchars($btnType); ?></a>
